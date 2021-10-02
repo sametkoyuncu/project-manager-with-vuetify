0a500cfc4ded09704b3e2ea3ec1ded4b4bde36a1
Samet Koyuncu <sametkoyuncu@live.com>
2021-10-02 11:18:43 +0300

    Add basic form validation for popup component

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,7 +2,14 @@
   <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn color="green accent-3" small dark v-bind="attrs" v-on="on">
+        <v-btn
+          color="green accent-3"
+          depressed
+          small
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
           YENİ PROJE EKLE
         </v-btn>
       </template>
@@ -10,7 +17,7 @@
         <v-card-title>
           <span class="text-h5">Yeni Proje Oluştur</span>
         </v-card-title>
-        <v-form class="px-3 pb-3">
+        <v-form class="px-3 pb-3" ref="form">
           <v-card-text>
             <v-container>
               <v-row>
@@ -20,6 +27,7 @@
                     v-model="title"
                     prepend-icon="mdi-folder"
                     hint="6-30 karakter uzunluğunda olmalı."
+                    :rules="[rules.required, rules.min, rules.max]"
                   >
                   </v-text-field>
                 </v-col>
@@ -38,7 +46,8 @@
                         prepend-icon="mdi-calendar"
                         v-bind="attrs"
                         v-on="on"
-                        @click:clear="dueDate = null"
+                        @click:clear="dueDate = ''"
+                        :rules="[rules.required]"
                       ></v-text-field>
                     </template>
                     <v-date-picker
@@ -55,6 +64,7 @@
                     v-model="content"
                     prepend-icon="mdi-file-edit"
                     hint="En az 10 karakter olmalı."
+                    :rules="[rules.required, rules.min, rules.max]"
                   >
                   </v-textarea>
                 </v-col>
@@ -80,13 +90,34 @@ export default {
     dialog: false,
     title: '',
     content: '',
-    dueDate: format(parseISO(new Date().toISOString()), 'yyyy-MM-dd'),
+    // dueDate: format(parseISO(new Date().toISOString()), 'yyyy-MM-dd'),
+    dueDate: '',
     menu: false,
+    rules: {
+      required: (value) => !!value || 'Bu alan gerekli.',
+      min: (value) =>
+        (value != null && value.length >= 6) || 'En az 6 karakter olmalı.',
+      max: (value) =>
+        (value != null && value.length <= 36) || 'En fazla 36 karakter olmalı.',
+      email: (value) => {
+        const pattern =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'Invalid e-mail.'
+      },
+    },
   }),
   methods: {
+    resetForm() {
+      Object.keys(this.form).forEach((f) => {
+        this.$refs[f].reset()
+      })
+    },
     submit() {
-      this.dialog = false
-      console.log(this.title + ' ' + this.content + ' ' + this.dueDate)
+      if (this.$refs.form.validate()) {
+        this.dialog = false
+        console.log(this.title + ' ' + this.content + ' ' + this.dueDate)
+        this.$refs.form.reset()
+      }
     },
   },
   computed: {
